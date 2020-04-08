2d759e610e701542a6e8f902c587c1dfad1aa13c
real-chiu <real.chiu@oocl.com>
2020-04-08 20:57:37 +0800

    [Real] passed test of should_reject_input_when_input_length_is_not_four

--- src/main/java/com/oocl/Validation.java
+++ src/main/java/com/oocl/Validation.java
@@ -3,7 +3,8 @@ package com.oocl;
 import java.util.ArrayList;
 
 public class Validation {
+    private static final int VALID_USER_INPUT_LENGTH = 4;
     public boolean checkIfInputLengthIsValid(ArrayList<Integer> parsedUserInput) {
-        return true;
+        return parsedUserInput.size() == VALID_USER_INPUT_LENGTH;
     }
 }
