94023c35db63ffe9330db1b3d6e5242d0e11c3df
real-chiu <real.chiu@oocl.com>
2020-04-08 18:25:57 +0800

    [Real] passed test of should_return_4A_when_digit_are_all_correct

--- src/main/java/com/oocl/CalculatorOfXAXB.java
+++ src/main/java/com/oocl/CalculatorOfXAXB.java
@@ -3,7 +3,18 @@ package com.oocl;
 import java.util.ArrayList;
 
 public class CalculatorOfXAXB {
+
     public String checkDigitCorrect(ArrayList<Integer> userInput, ArrayList<Integer> answer) {
+//        int finalCorrectDigitCount = userInput.stream().reduce(0, (intermediateCorrectDigitCount, currentDigit) -> {
+//            if (answer.contains(currentDigit)) {
+//                return ++intermediateCorrectDigitCount;
+//            }
+//            return intermediateCorrectDigitCount;
+//        });
+        boolean allCorrect = userInput.stream().allMatch(digit -> answer.contains(digit));
+        if (allCorrect) {
+            return "4A";
+        }
         return "";
     }
 }
