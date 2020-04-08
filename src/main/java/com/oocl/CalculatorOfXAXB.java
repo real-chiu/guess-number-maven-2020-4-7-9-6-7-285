bd71e35712b216587c540b1c6da5e7c921c17d00
real-chiu <real.chiu@oocl.com>
2020-04-08 18:33:02 +0800

    [Real] passed test of should_return_XA_when_digit_are_partially_correct; Code refractor;

--- src/main/java/com/oocl/CalculatorOfXAXB.java
+++ src/main/java/com/oocl/CalculatorOfXAXB.java
@@ -5,20 +5,12 @@ import java.util.ArrayList;
 public class CalculatorOfXAXB {
 
     public String checkDigitCorrect(ArrayList<Integer> userInput, ArrayList<Integer> answer) {
-//        int finalCorrectDigitCount = userInput.stream().reduce(0, (intermediateCorrectDigitCount, currentDigit) -> {
-//            if (answer.contains(currentDigit)) {
-//                return ++intermediateCorrectDigitCount;
-//            }
-//            return intermediateCorrectDigitCount;
-//        });
-        boolean allCorrect = userInput.stream().allMatch(digit -> answer.contains(digit));
-        boolean allNotCorrect = userInput.stream().allMatch(digit -> !answer.contains(digit));
-        if (allCorrect) {
-            return "4A";
-        }
-        if (allNotCorrect) {
-            return "0A";
-        }
-        return "";
+        int finalCorrectDigitCount = userInput.stream().reduce(0, (intermediateCorrectDigitCount, currentDigit) -> {
+            if (answer.contains(currentDigit)) {
+                return ++intermediateCorrectDigitCount;
+            }
+            return intermediateCorrectDigitCount;
+        });
+        return Integer.toString(finalCorrectDigitCount) + "A";
     }
 }
