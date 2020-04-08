3864e1059e41ec1cff9cbd5b11bddad71168466a
real-chiu <real.chiu@oocl.com>
2020-04-08 18:30:18 +0800

    [Real] passed test of should_return_0A_when_digit_are_all_not_correct

--- src/main/java/com/oocl/CalculatorOfXAXB.java
+++ src/main/java/com/oocl/CalculatorOfXAXB.java
@@ -12,11 +12,13 @@ public class CalculatorOfXAXB {
 //            return intermediateCorrectDigitCount;
 //        });
         boolean allCorrect = userInput.stream().allMatch(digit -> answer.contains(digit));
-
+        boolean allNotCorrect = userInput.stream().allMatch(digit -> !answer.contains(digit));
         if (allCorrect) {
             return "4A";
         }
-
+        if (allNotCorrect) {
+            return "0A";
+        }
         return "";
     }
 }
