d76e6f2a6c7d7bab5272a33d38431176a37b4091
real-chiu <real.chiu@oocl.com>
2020-04-08 18:13:15 +0800

    supplement test case should_return_4A_when_digit_are_all_correct

--- src/main/java/com/oocl/CalculatorOfXAXB.java
+++ src/main/java/com/oocl/CalculatorOfXAXB.java
@@ -3,7 +3,7 @@ package com.oocl;
 import java.util.ArrayList;
 
 public class CalculatorOfXAXB {
-    public String checkDigitCorrect(ArrayList<Integer> userInput) {
+    public String checkDigitCorrect(ArrayList<Integer> userInput, ArrayList<Integer> answer) {
         return "";
     }
 }
--- src/test/java/com/oocl/CalculatorOfXAXBTest.java
+++ src/test/java/com/oocl/CalculatorOfXAXBTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class CalculatorOfXAXBTest {
     private CalculatorOfXAXB calculatorOfXAXB;
     private ArrayList<Integer> userInput;
+    private ArrayList<Integer> answer;
     @Before
     public void setUp() throws Exception {
         calculatorOfXAXB = new CalculatorOfXAXB();
@@ -17,11 +18,17 @@ public class CalculatorOfXAXBTest {
         userInput.add(2);
         userInput.add(3);
         userInput.add(4);
+
+        answer = new ArrayList<Integer>();
+        answer.add(1);
+        answer.add(2);
+        answer.add(3);
+        answer.add(4);
     }
 
     @Test
     public void should_return_4A_when_digit_are_all_correct() {
-        String result = calculatorOfXAXB.checkDigitCorrect(userInput);
+        String result = calculatorOfXAXB.checkDigitCorrect(userInput, answer);
         Assert.assertEquals("4A", result);
     }
 }
